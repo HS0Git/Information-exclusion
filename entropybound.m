90fb415c32f9691e37afa0e8cf21b1ed81d08636
HS0Git <124451231+HS0Git@users.noreply.github.com>
2023-02-05 23:46:55 +0800

    Update entropybound.m

--- entropybound.m
+++ entropybound.m
@@ -21,7 +21,7 @@ parfor n=1:1000
       e1=T;e2=T;%temporary variables
      for m=1:1000 %minimize over random states
          state=diag([1 zeros(1,d-1)]);
-         v=2*pi*rand(1,d^2-1);v=reshape(v,[1,1,d^2-1]);u=expm(1i*sum(times(v,A),3));
+         v=2*pi*rand(1,d^2-1).^2;v=reshape(v,[1,1,d^2-1]);u=expm(1i*sum(times(v,A),3));
          state=transpose(conj(u))*state*u;
          p=diag(conj(B)*state*transpose(B));p=abs(transpose(p));%probability vector for measurement outcomes
          s=-p*transpose(log2(p+eps));%the sum of shannon entropies
@@ -38,17 +38,14 @@ parfor n=1:1000
      entropys=[entropys e1];
 end
 sz=30;figure,s=0:0.01:T-1;
+scatter(IU,entropys,sz,[1,0.5,0],'.'), hold on;
+%lower bound on Shannon entropy given by Eq.(20) of the main text//IC=T/d+(T-IU)(1-1/d)
+plot(s,SentropyIC(T/d+(T-s)*(1-1/d),T),'b--','linewidth',1.13), hold on;
 scatter(IU,entropy2,sz,[.55,0.55,.55],'.'), hold on;
 %lower bound on Renyi-2 entropy given by Eq.(19) of the main text
-plot(s,-T*log2((1/d+(T-s)/T/d)),'r','linewidth',1.13), 
-hold on;
-scatter(IU,entropys,sz,[1,0.5,0],'.'), hold on;
-%lower bound on Shannon entropy given by Eq.(19) of the main text//IC=T/d+(T-IU)(1-1/d)
-plot(s,SentropyIC(T/d+(T-s)*(1-1/d),T),'b--','linewidth',1.13), hold on;
+plot(s,-T*log2((1/d+(T-s)/T/d)),'r','linewidth',1.13), hold on;
 
 grid on;xlabel('Index of uncertainty');ylabel('Sum of entropies');
-%axis([0,T-1,0,T*log2(d)]);
-axis([0,T-1,0,2]);
 set(get(gca,'XLabel'),'FontSize',16);
 set(get(gca,'YLabel'),'FontSize',16);
 set(get(gca,'TITLE'),'FontSize',15);
